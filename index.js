684d7d94bfe0e533a45b5d63459c555841a9d39b
kashika24 <flyh80166@gmail.com>
2025-02-02 17:51:05 +0530

    Added env file

--- index.js
+++ index.js
@@ -201,6 +201,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 
 const app = express();
 app.use(express.json());
